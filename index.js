7bf90249f6ed3df223690a0c92584257371ba7f7
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-12 10:41:22 +0500

    updated practice files

--- index.js
+++ index.js
@@ -1,94 +1,119 @@
 function Navbar() {
-   return (
-       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-           <a className="navbar-brand" href="#">Navbar</a>
-           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span className="navbar-toggler-icon"></span>
-           </button>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul className="navbar-nav mr-auto">
-               <li className="nav-item active">
-                   <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-               </li>
-               <li className="nav-item">
-                   <a className="nav-link" href="#">Link</a>
-               </li>
-               <li className="nav-item dropdown">
-                   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                   Dropdown
-                   </a>
-                   <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                   <a className="dropdown-item" href="#">Action</a>
-                   <a className="dropdown-item" href="#">Another action</a>
-                   <div className="dropdown-divider"></div>
-                   <a className="dropdown-item" href="#">Something else here</a>
-                   </div>
-               </li>
-               <li className="nav-item">
-                   <a className="nav-link disabled" href="#">Disabled</a>
-               </li>
-               </ul>
-               <form className="form-inline my-2 my-lg-0">
-                   <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                   <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-               </form>
-           </div>
-       </nav>
-   )
-}
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
+ }
 
-function MainContent() {
-   return (
-       <h1>This is my first react component</h1>
-   )
-}
+ function MainContent() {
+    return (
+        <h1>This is my first react component</h1>
+    )
+ }
 
-// Challenge: Create your own custom React component!
-// Call it "MainContent", and have it return a simple
-// h1 element that says "I'm learning React!"
+ // Challenge: Create your own custom React component!
+ // Call it "MainContent", and have it return a simple
+ // h1 element that says "I'm learning React!"
 
-// Afterward, render it below the Navbar (which
-// you can do inside the ReactDOM.render call below)
+ // Afterward, render it below the Navbar (which
+ // you can do inside the ReactDOM.render call below)
 
-ReactDOM.render(
-   <div>
-       <Navbar />
-       <MainContent />
-   </div>,
-   document.getElementById("root")
-)
+ ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+ )
 
-// const page = (
-//     <div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
+ // const page = (
+ //     <div>
+ //         <h1 className="header">This is JSX</h1>
+ //         <p>This is a paragraph</p>
+ //     </div>
+ // )
 
-/*
-Challenge:
+ /*
+ Challenge:
 
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
+ Create a navbar in JSX:
+     - Use the semantic `nav` element as the parent wrapper
+     - Have an h1 element with the brand name of your "website"
+     - Insert an unordered list for the other nav elements
+         - Inside the `ul`, have three `li`s for "Pricing",
+         "About", and "Contact"
+     - Don't worry about styling yet - it'll just be plain-looking HTML for now
+ */
 
-let navbar = (
-   <nav>
-   <h1>MyLogo</h1>
-   <ul>
-   <li>Pricing</li>
-   <li>About</li>
-   <li>Contact</li>
-   </ul>
-   </nav>
-)
-let root= document.getElementById("root")
-ReactDOM.render(
-   navbar,root
-)
+ let navbar = (
+    <nav>
+    <h1>MyLogo</h1>
+    <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+    </ul>
+    </nav>
+ )
+ let root= document.getElementById("root")
+ ReactDOM.render(
+    navbar,root
+ )
+ import React from "react"
+ import ReactDOM from "react-dom"
+
+ /**
+ Challenge: fix our code!
+
+ Don't forget, you're not using CDNs anymore, so there's no
+ global "ReactDOM" variable to use anymore.
+  */
+
+ const page = (
+     <div>
+         <h1>My awesome website in React</h1>
+         <h3>Reasons I love React</h3>
+         <ol>
+             <li>It's composable</li>
+             <li>It's declarative</li>
+             <li>It's a hireable skill</li>
+             <li>It's actively maintained by skilled people</li>
+         </ol>
+     </div>
+ )
+
+ ReactDOM.render(page, document.getElementById("root"))
+ // document.getElementById("root").append(JSON.stringify(page))
